5cfba4911f8045e5e0a67f799a00963f2aecac53
Eric Baer <me@ericbaer.com>
2015-04-28 14:01:50 -0700

    Enforce name attribute in config

--- test/spec/lib/tests/_baseTest.spec.js
+++ test/spec/lib/tests/_baseTest.spec.js
@@ -3,14 +3,19 @@ var _baseTest = require('../../../../lib/tests/_baseTest.js');
 
 describe("tests/_baseTest", function () {
   describe('configuration', function() {
-    it('should create a config property when no config is passed in', function() {
+    it('should throw an Error when not given a name', function() {
       var FakeTester = _baseTest();
-      var fakeTester = new FakeTester();
+      var error = false;
 
-      assert(_.isEqual(fakeTester.config, {}));
+      try {
+        var fakeTester = new FakeTester();
+      } catch (e) {
+        error = true;
+      }
+      assert(error);
     });
     it('should create a config property when only runtimeConfig is passed in', function() {
-      var runtimeTestConfig = { runtime: true };
+      var runtimeTestConfig = { name: 'test', runtime: true };
 
       var FakeTester = _baseTest();
       var fakeTester = new FakeTester(runtimeTestConfig);
@@ -18,7 +23,7 @@ describe("tests/_baseTest", function () {
       assert(_.isEqual(fakeTester.config, runtimeTestConfig));
     });
     it('should create a config property when only baseConfig is passed in', function() {
-      var baseTestConfig = { base: true };
+      var baseTestConfig = { name: 'test', base: true };
 
       var FakeTester = _baseTest(baseTestConfig);
       var fakeTester = new FakeTester();
@@ -27,6 +32,7 @@ describe("tests/_baseTest", function () {
     });
     it('should not modify the config objects', function() {
       var baseTestConfig = {
+        name: 'test',
         base: true,
         deep: {
           common: "base",
@@ -51,6 +57,7 @@ describe("tests/_baseTest", function () {
     });
     it('should contain unique (deep) values from both dictionaries', function() {
       var baseTestConfig = {
+        name: 'test',
         base: true,
         deep: {
           common: "base",
@@ -75,13 +82,13 @@ describe("tests/_baseTest", function () {
   });
   describe('default test method', function() {
     it('should have a test method', function() {
-      var FakeTester = _baseTest();
+      var FakeTester = _baseTest({name: 'test'});
       var fakeTester = new FakeTester();
 
       assert(_.isFunction(fakeTester.test));
     });
     it('should throw an Error when executed', function() {
-      var FakeTester = _baseTest();
+      var FakeTester = _baseTest({name: 'test'});
       var fakeTester = new FakeTester();
 
       var error = false;
